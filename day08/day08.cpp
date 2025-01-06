ffbf207c2df55c929690fde9526c1dfb1a232435
lurk101 <lurkone0one@gmail.com>
2025-01-06 11:35:27 -0500

    day8

--- day08/day08.cpp
+++ day08/day08.cpp
@@ -20,7 +20,7 @@ static bool inside(const pair<int, int>& p) {
     return p.first >= 0 && p.first < maxRow && p.second >= 0 && p.second < maxCol;
 }
 
-static int nodes() {
+static auto nodes() {
     set<pair<int, int>> n;
     for (const auto& [point, kind] : ants) {
         for (const auto& [other, otherKind] : ants) {
@@ -30,10 +30,10 @@ static int nodes() {
             if (inside(p)) n.insert(p);
         }
     }
-    return int(n.size());
+    return n.size();
 }
 
-static int harmonics() {
+static auto harmonics() {
     set<pair<int, int>> n;
     for (const auto& [point, kind] : ants) {
         for (const auto& [other, otherKind] : ants) {
@@ -47,15 +47,13 @@ static int harmonics() {
                 n.insert(p);
         }
     }
-    return int(n.size());
+    return n.size();
 }
 
 int main() {
     stdio_init_all();
     auto start = high_resolution_clock::now();
-    ifstream fi("day08.txt");
-    string line;
-    while (getline(fi, line)) {
+    for (const auto& line : lines) {
         int col = 0;
         for (const auto& c : line) {
             if (c != '.') ants.try_emplace({maxRow, col}, c);

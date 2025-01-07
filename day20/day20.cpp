ce910fc96b993105a7e2d34cdb471a495e224b37
lurk101 <lurkone0one@gmail.com>
2025-01-07 16:46:52 -0500

    day20

--- day20/day20.cpp
+++ day20/day20.cpp
@@ -10,69 +10,67 @@
 using namespace std;
 using namespace chrono;
 
-static const vector<string> lines = {
+static vector<string> grid = {
 #include "day20.txt"
 };
 
-struct Point {
+static int height, width;
+
+struct point {
     int y, x;
-    bool operator==(const Point& p) const { return (y == p.y) && (x == p.x); }
-    Point operator+(const Point& p) const { return {y + p.y, x + p.x}; }
+    bool operator==(const point& p) const { return (y == p.y) && (x == p.x); }
+    point operator+(const point& p) const { return {y + p.y, x + p.x}; }
+    inline bool inside() const { return x >= 0 && x < width && y >= 0 && y < height; }
 };
 
-static vector<string> grid;
-static int height, width;
-static Point start, finish;
-static const vector<Point> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+static point start, finish;
+static const vector<point> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-static auto Search(vector<string>& grid, Point& start, Point& finish) {
+static auto search(vector<string>& grid, point& start, point& finish) {
     vector<vector<int>> dist(height, vector<int>(width, -1));
-    vector<vector<bool>> visited(height, vector<bool>(width, false));
-    queue<pair<Point, int>> q;
+    queue<pair<point, int>> q;
     q.push({start, 0});
-    visited[start.y][start.x] = true;
     dist[start.y][start.x] = 0;
     while (!q.empty()) {
         auto [cur, d] = q.front();
         q.pop();
         if (cur == finish) break;
         for (auto dir : dirs) {
-            auto [y, x] = cur + dir;
-            if (x < 0 || x >= width || y < 0 || y >= height || visited[y][x] || grid[y][x] == '#')
-                continue;
-            visited[y][x] = true;
-            dist[y][x] = d + 1;
-            q.push({{y, x}, d + 1});
+            auto p = cur + dir;
+            if ((!p.inside()) || (dist[p.y][p.x] >= 0) || (grid[p.y][p.x] == '#')) continue;
+            dist[p.y][p.x] = d + 1;
+            q.push({p, d + 1});
         }
     }
     return dist;
 }
 
-static inline int Manhattan(Point a, Point b) { return abs(a.y - b.y) + abs(a.x - b.x); }
+static inline auto manhattan(const point& a, const point& b) {
+    return abs(a.y - b.y) + abs(a.x - b.x);
+}
 
-static int Part(int steps) {
+static auto part(int steps) {
     int s = 0;
-    vector<vector<int>> toStart = Search(grid, start, finish),
-                        toFinish = Search(grid, finish, start);
+    vector<vector<int>> toStart = search(grid, start, finish),
+                        toFinish = search(grid, finish, start);
     int t = toStart[finish.y][finish.x];
-    map<pair<Point, Point>, bool> checked;
-    for (int y = 0; y < height; y++) {
+    map<pair<point, point>, bool> checked;
+    for (int y = 0; y < height; y++)
         for (int x = 0; x < width; x++) {
             if (grid[y][x] == '#') continue;
             for (int newY = max(0, y - steps); newY <= min(height, y + steps); newY++) {
                 if (newY < 0 || newY >= height) continue;
                 for (int newX = max(0, x - steps); newX <= min(width, x + steps); newX++) {
                     if ((newX < 0 || newX >= width) || (grid[newY][newX] == '#') ||
-                        (Manhattan({y, x}, {newY, newX}) > steps) ||
+                        (manhattan({y, x}, {newY, newX}) > steps) ||
                         (toStart[y][x] < 0 || toFinish[newY][newX] < 0))
                         continue;
                     int cheat =
-                        toStart[y][x] + toFinish[newY][newX] + Manhattan({y, x}, {newY, newX});
+                        toStart[y][x] + toFinish[newY][newX] + manhattan({y, x}, {newY, newX});
                     if (t - cheat >= 100) s++;
                 }
             }
         }
-    }
     return s;
 }
 
@@ -81,9 +79,7 @@ int main() {
     auto strt = high_resolution_clock::now();
     string line;
     height = 0;
-    ifstream fi("day20.txt");
-    while (getline(fi, line)) {
-        grid.push_back(line);
+    for (const auto& line : grid) {
         for (int x = 0; x < line.size(); x++)
             if (line[x] == 'S')
                 start = {height, x};
@@ -93,8 +89,8 @@ int main() {
     }
     width = int(grid[0].size());
     cout << "Day 20: Race Condition" << endl
-         << "Part 1   - " << Part(2) << endl
-         << "Part 2   - " << Part(20) << endl
+         << "Part 1   - " << part(2) << endl
+         << "Part 2   - " << part(20) << endl
          << "run time - "
          << duration_cast<microseconds>(high_resolution_clock::now() - strt).count() / 1000.0
          << " ms." << endl;
